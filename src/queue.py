ff23ca7cc8605ee911fcb3b856e44d7d3750e21b
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2016-07-20 19:03:18 +1200

    add collect metrics in controller

--- src/queue.py
+++ src/queue.py
@@ -31,6 +31,7 @@ class QueueController(object):
         # log options processed
         all_options = ["  %s -> %s" % (key, value) for key, value in conf.copy().items()]
         self.logger.debug("Configuration:\n%s" % "\n".join(all_options))
+        self.session = self._create_session(conf)
 
     def _create_session(self, conf):
         """
@@ -44,10 +45,40 @@ class QueueController(object):
         # currently we only have one session for Spark, default logger is used for now
         return spark.SparkSession.create(conf, None)
 
+    def get_status_dict(self):
+        """
+        Get dictionary of all statuses, metrics from session and scheduler.
+
+        :return: dict of statuses and metrics
+        """
+        session_status = {}
+        # session metrics
+        session_status["code"] = self.session.system_code()
+        # system URI can be None
+        sys_uri = self.session.system_uri()
+        session_status["url"] = {"href": sys_uri.url, "alias": sys_uri.alias} if sys_uri else None
+        session_status["status"] = self.session.status()
+        # scheduler metrics
+        session_status["scheduler"] = {
+            "name": type(self.session.scheduler).__name__,
+            "num_executors": self.session.scheduler.get_num_executors(),
+            "executor_class": self.session.scheduler.executor_class(),
+            "metrics": self.session.scheduler.get_metrics(),
+            "is_alive_statuses": self.session.scheduler.get_is_alive_statuses()
+        }
+        return session_status
+
     @cherrypy.expose
     def index(self):
         template = env.get_template(TEMPLATE_HOME)
         return template.render()
+
+    def start(self):
+        """
+        Start all session services.
+        """
+        self.session.scheduler.start_maintenance()
+        self.session.scheduler.start()
 
 # Configuration setup for application except host and port
 def getConf(): # pragma: no cover
@@ -74,4 +105,6 @@ def start(host="127.0.0.1", port=8080, args=None): # pragma: no cover
     """
     cherrypy.config.update({"server.socket_host": host})
     cherrypy.config.update({"server.socket_port": port})
-    cherrypy.quickstart(QueueController(args), "/", getConf())
+    controller = QueueController(args)
+    controller.start()
+    cherrypy.quickstart(controller, "/", getConf())
--- test/test_queue.py
+++ test/test_queue.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+import unittest
 import cherrypy
 import mock
+import src.const as const
 import src.queue as queue
+import src.spark as spark
 import src.util as util
 from test.cptestcase import BaseCherryPyTestCase
 
 def setUpModule():
-    controller = queue.QueueController(args={}, logger=mock.Mock())
-    cherrypy.tree.mount(controller, "/", queue.getConf())
+    # for testing web-service functionality only we mock session in controller
+    with mock.patch.object(queue.QueueController, "_create_session", return_value=mock.Mock()):
+        controller = queue.QueueController(args={}, logger=mock.Mock())
+        cherrypy.tree.mount(controller, "/", queue.getConf())
     cherrypy.engine.start()
 setup_module = setUpModule
 
@@ -16,19 +21,11 @@ def tearDownModule():
     cherrypy.engine.exit()
 teardown_module = tearDownModule
 
-# pylint: disable=W0212,protected-access
-class QueueControllerSuite(BaseCherryPyTestCase):
-    @mock.patch("src.queue.spark")
-    def test_create_session(self, mock_spark):
-        controller = queue.QueueController(args={}, logger=mock.Mock())
-        with self.assertRaises(AttributeError):
-            controller._create_session(None)
-        with self.assertRaises(AttributeError):
-            controller._create_session({"a": 1})
-        conf = util.QueueConf()
-        controller._create_session(conf)
-        mock_spark.SparkSession.create.assert_called_once_with(conf, None)
-
+class QueueSuite(BaseCherryPyTestCase):
+    """
+    Testing suite for web-service functionality only. All internal methods that are not related to
+    REST API or serving web-pages, should be in controller suite.
+    """
     def test_template(self):
         self.assertEqual(queue.TEMPLATE_HOME, "home.html")
 
@@ -37,10 +34,61 @@ class QueueControllerSuite(BaseCherryPyTestCase):
         self.assertEqual(response.output_status, "200 OK")
         self.assertEqual(response.headers["Content-Type"], "text/html;charset=utf-8")
         self.assertTrue("<title>Home &middot; Queue</title>" in response.body[0])
+
+# == Test session + scheduler ==
+test_scheduler = mock.create_autospec(spark.SparkStandaloneScheduler, spec_set=True, instance=True)
+test_scheduler.get_num_executors.return_value = 5
+test_scheduler.executor_class.return_value = spark.SparkStandaloneExecutor
+test_scheduler.get_metrics.return_value = {"a": 1, "b": 2}
+test_scheduler.get_is_alive_statuses.return_value = {"ex1": util.utcnow(), "ex2": util.utcnow()}
+
+test_session = mock.create_autospec(spark.SparkSession, spec_set=True, instance=True)
+test_session.system_code.return_value = "TEST"
+test_session.system_uri.return_value = util.URI("http://local:8080", "link")
+test_session.status.return_value = const.SYSTEM_BUSY
+test_session.scheduler = test_scheduler
+
+# pylint: disable=W0212,protected-access
+class QueueControllerSuite(unittest.TestCase):
+    def setUp(self):
+        test_scheduler.reset_mock()
+        test_session.reset_mock()
+
+    @mock.patch.object(spark.SparkSession, "create", return_value=test_session)
+    def test_create_session(self, mock_session): # pylint: disable=W0613,unused-argument
+        controller = queue.QueueController(args={}, logger=mock.Mock())
+        with self.assertRaises(AttributeError):
+            controller._create_session(None)
+        with self.assertRaises(AttributeError):
+            controller._create_session({"a": 1})
+        conf = util.QueueConf()
+        session = controller._create_session(conf)
+        self.assertNotEqual(session, None)
+
+    @mock.patch.object(spark.SparkSession, "create", return_value=test_session)
+    def test_get_status_dict(self, mock_session): # pylint: disable=W0613,unused-argument
+        controller = queue.QueueController(args={}, logger=mock.Mock())
+        metrics = controller.get_status_dict()
+        self.assertEqual(metrics["code"], "TEST")
+        self.assertEqual(metrics["url"], {"href": "http://local:8080", "alias": "link"})
+        self.assertEqual(metrics["status"], const.SYSTEM_BUSY)
+        self.assertEqual(metrics["scheduler"]["executor_class"], spark.SparkStandaloneExecutor)
+        self.assertEqual(metrics["scheduler"]["num_executors"], 5)
+        self.assertEqual(metrics["scheduler"]["metrics"], {"a": 1, "b": 2})
+        self.assertTrue("ex1" in metrics["scheduler"]["is_alive_statuses"])
+        self.assertTrue("ex2" in metrics["scheduler"]["is_alive_statuses"])
+
+    @mock.patch.object(spark.SparkSession, "create", return_value=test_session)
+    def test_start(self, mock_session): # pylint: disable=W0613,unused-argument
+        controller = queue.QueueController(args={}, logger=mock.Mock())
+        controller.start()
+        test_scheduler.start_maintenance.assert_called_once_with()
+        test_scheduler.start.assert_called_once_with()
 # pylint: enable=W0212,protected-access
 
 # Load test suites
 def suites():
     return [
+        QueueSuite,
         QueueControllerSuite
     ]
